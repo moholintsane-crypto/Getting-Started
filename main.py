2c2480217784a84b770f06bf0af75025404eb2e9
moholintsane-crypto <moholine1@gmail.com>
2026-02-06 18:09:13 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+print("hello")
+print(8)
+#this is a comment
+print("I am of age",7)
+print(18+8)
+
+import keyword
+print(keyword.kwlist)
---
+++ variable.py
@@ -0,0 +1,7 @@
+age=10
+grade=9
+
+print("my age is",age)
+
+name=input("what is your name? ")
+print("hello",name)
